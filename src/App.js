6806cb8b47c47f8b82b03edd22bf369f42a77178
Chetan-Salunke <chetanbusalunke@gmail.com>
2024-06-25 23:56:51 +0530

    card-section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <>
     <Header/>
+    <Cards/>
     </>
   );
 }
