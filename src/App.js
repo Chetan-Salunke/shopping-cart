7cbd58e7d1a0c0f367726e8638e0584b13152ff3
Chetan-Salunke <chetanbusalunke@gmail.com>
2024-06-26 23:58:25 +0530

    cart-section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Cards from './components/Cards';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <>
     <Header/>
     <Cards/>
+    <Cart/>
     </>
   );
 }
